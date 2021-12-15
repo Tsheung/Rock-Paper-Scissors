ad03d34cb2c9169a62fda81f353b78ca19053b8b
Tsheung <tobylsheung@gmail.com>
2021-12-14 22:24:31 -0500

    Add caseInsensitive function game.js

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
+//Computer chooses randomly
 function computerPlay() {
     let choices = ["Rock", "Paper", "Scissors"];
     let num = Math.floor(Math.random() * 3);
     return choices[num];
 }
 
+//playerSelection input is case-insensitive
+function caseInsensitive(str) {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+//Win vs Lose
 function playRound(playerSelection, computerSelection) {
+    //inputs playerselection to not sensitive
+    playerSelection = caseInsensitive(playerSelection);
+
     if (playerSelection === computerSelection) {
         return `You Tied! Both of you chose ${playerSelection}`; 
     } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
@@ -20,11 +30,12 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
-
 }
 
 
-const playerSelection = "Scissors";
+
+
+const playerSelection = "sCISSORS";
 const computerSelection = computerPlay();
 
 console.log(playRound(playerSelection, computerSelection));
