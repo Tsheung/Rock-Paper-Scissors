be0964f474770c2a9f51c5b0d5cff132b514717a
Tsheung <tobylsheung@gmail.com>
2021-12-14 22:15:59 -0500

    Add playRound function

--- game.js
+++ game.js
@@ -4,4 +4,27 @@ function computerPlay() {
     return choices[num];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `You Tied! Both of you chose ${playerSelection}`; 
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+}
+
+
+const playerSelection = "Scissors";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
