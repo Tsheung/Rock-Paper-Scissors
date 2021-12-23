6dd577b1209487cdcb5dea76485c0bd6ae078a29
Tsheung <tobylsheung@gmail.com>
2021-12-22 23:45:45 -0500

    Add game function into dom

--- game.js
+++ game.js
@@ -32,40 +32,42 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-//Plays Until 5 points
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-
-    while (playerPoints < 5 && computerPoints < 5) {
-        let playerSelection = prompt("What is your choice?");
-        let computerSelection = computerPlay();
-
-        //Return win or lose
-        let msg = playRound(playerSelection, computerSelection);
-        
-        //Adds total points
-        if (msg.includes("Win") == true) {
-            playerPoints +=1;
-        } else if (msg.includes("Lose") == true) {
-            computerPoints +=1;
-        } 
-
-        //Print msg
-        console.log(msg);
-        console.log(`You: ${playerPoints}, PC: ${computerPoints}`);
-    }
-}
 
 //retrieve all buttons
 const buttons = document.querySelectorAll('.Images div');
-console.log(buttons);
 
+//Count
+let playerPoints = 0;
+let computerPoints = 0;
+
+//Click Response
 buttons.forEach((b) => {
     b.addEventListener('click', () => {
-        console.log(b.className);
+        //console.log(b.className);
+        
+        if (playerPoints < 5 && computerPoints < 5) {
+            let computerSelection = computerPlay();
+            let playerSelection = b.className;
+    
+            //Return win or lose
+            let msg = playRound(playerSelection, computerSelection);
+            
+            //Adds total points
+            if (msg.includes("Win") == true) {
+                playerPoints +=1;
+            } else if (msg.includes("Lose") == true) {
+                computerPoints +=1;
+            } 
+
+            console.log(msg);
+            console.log(`You: ${playerPoints}, PC: ${computerPoints}`);
+        }
+
     });
 });
 
 
-//game();
+//buttons.onclick = function () {}
+
+
+
